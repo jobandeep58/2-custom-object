7cacd7380c41a5a1f694a0f6854c88b9c7a05204
jobandeep singh <10009573@student.toiohomai.ac.nz>
2019-03-14 12:01:25 +1300

    uml

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class person
+    {
+        static void Main(string[] args)
+        {
+            string fname;
+            string lname;
+            int age;
+            int counter;
+        }
+        public string Fname
+        {
+            get { return Fname; }
+            set {fname= value; }
+
+        }
+        public string Lname
+        {
+            get { return Lname; }
+            set { return Fname; }
+
+        }
+        public string Fname
+        {
+            get { return Fname; }
+            set { return Lname; }
+
+        }
+        public string Fname
+        {
+            get { return Fname; }
+            set { return Lname; }
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string fname;
+            string lname;
+            int age;
+            int counter;
+        }
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+
+        }
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+
+        }
+
+
+    }
+}
+     
